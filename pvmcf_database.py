8e1ecebd20ce4af676de46e30f7c28d9398a35f1
Brent Thompson <brent.thompson@ucf.edu>
2025-02-19 15:51:35 -0500

    Basic Code snippets and modules

---
+++ pvmcf_database.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 12:32:37 2024
+
+@author: Brent Thompson
+@
+"""
+
+import sqlite3
+import json
+
+
+class PVMCF_Database:
+    def __init__(self, filepath, schema_file):
+        self.filepath = filepath
+
+
+# Connect to the database to preform all lab operations
+DATABASE_FILEPATH = ""
+
+
+#PVMCF_DB = sqlite3.connect(DATABASE_FILEPATH)
+# cursor = PVMCF_DB.cursor()
+
+
+"C:/UCF/FSEC/Data/FSEC_PVMCF/module_databases/PVMCF_Database.db"
